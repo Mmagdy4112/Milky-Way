5759259c6cf6612016843641d6c2f8ef0067a4b6
m.magdy4112gmail.com <m.magdy4112gmail.com>
2021-11-20 14:12:16 +0200

    add ui files

---
+++ app/src/main/java/app/milkyway/ui/fragments/MilkyImages/MilkyImages.kt
@@ -0,0 +1,19 @@
+package app.milkyway.ui.fragments.MilkyImages
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import app.milkyway.R
+
+class MilkyImages : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_milky_images, container, false)
+    }
+}
